c1314f01efb888d7a3c7e73a3232d43d6e4f6223
nekoulahaddad <nekoulahaddad@gmail.com>
2023-05-26 22:24:50 +0300

    deploy config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import EnhancedTable from "./components/EnhancedTable";
 import WrapperCompoenent from "./components/WrapperComponent";
 import DragDrop from "./components/DragAndDrop";
 
